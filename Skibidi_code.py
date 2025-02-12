50308abeab8ff51809d3df26ba1b3e6e50eb1ac3
SkibidiSigma228ez <pavelvelikiy.10.18@gmail.com>
2025-02-12 21:28:42 +0300

    Update Skibidi_code.py
    Added a new command and a new dictionary

--- Skibidi_code.py
+++ Skibidi_code.py
@@ -1,14 +1,29 @@
-import time, threading, schedule
+import time, threading, schedule, random
 from telebot import TeleBot
 from bot_logic import gen_pass
-
+skibididecayingitems = {
+    "батарейка": "Батарейка разлагается от 100 до 1000 лет. Переработка важна!",
+    "яблоко": "Яблоко разлагается за 2-4 недели.",
+    "пластик": "Пластик разлагается от 100 до 1000 лет. Переработка важна!",
+    "стекло": "Стекло разлагается более 1000 лет, но его можно перерабатывать бесконечно.",
+    "алюминиевая банка": "Алюминиевая банка разлагается около 500 лет. Лучше сдать на переработку!",
+    "бумага": "Бумага разлагается от нескольких недель до 2 лет.",
+    "дерево": "Дерево разлагается от 2 до 10 лет в зависимости от условий.",
+    "резина": "Резина разлагается более 100 лет. Шины лучше сдавать в переработку.",
+    "полиэтиленовый пакет": "Полиэтиленовый пакет разлагается от 100 до 400 лет.",
+    "жестяная банка": "Жестяная банка разлагается около 50 лет.",
+    "кожура банана": "Кожура банана разлагается за 2-5 недель.",
+    "картон": "Картон разлагается примерно за 2 месяца.",
+    "фольга": "Фольга практически не разлагается, но может быть переработана.",
+    "железо": "Железо разлагается около 10 лет, но может ржаветь быстрее."
+}
 API_TOKEN = '[REDACTED]'
 bot = TeleBot(API_TOKEN)
 
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Используй /set <секунды> чтобы поставить таймер")
+    bot.reply_to(message, "Привет! Используй '/hello' '/bye' '/gen' '/photo' и '/set <секунды>' с '/unset' чтобы поставить или убрать таймер")
 
 
 def beep(chat_id) -> None:
@@ -31,12 +46,6 @@ def unset_timer(message):
     schedule.clear(message.chat.id)
 
 
-if __name__ == '__main__':
-    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
 @bot.message_handler(commands=['hello'])
 def send_hello(message):
     bot.reply_to(message, "Привет! Как дела?")
@@ -49,7 +58,20 @@ def send_bye(message):
 def gen(message):
     password = gen_pass(10) 
     bot.reply_to(message, password) 
-    
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+
+@bot.message_handler(commands=['photo'])
+def send_cat(message):
+    images = ['images/cat.jpg', 'images/cat2.jpg', 'images/cat3.jpg']
+    with open(random.choice(images), 'rb') as f:
+        bot.send_photo(message.chat.id, f)
+
+@bot.message_handler(commands=['decay'])
+def send_decay(message):
+    random_item = random.choice(list(skibididecayingitems.keys()))
+    bot.reply_to(message, skibididecayingitems[random_item])
+
+if __name__ == '__main__':
+    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
